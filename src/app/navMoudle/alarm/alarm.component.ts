dc3138d8574d9c2b0660c4b23c8e6f0a20d1159a
wuzhiwei <52383319@qq.com>
2018-11-06 15:24:27 +0800

    完善顶部导航的“警告”组件

--- src/app/navMoudle/alarm/alarm.component.ts
+++ src/app/navMoudle/alarm/alarm.component.ts
@@ -1,13 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-alarm',
   templateUrl: './alarm.component.html',
-  styleUrls: ['./alarm.component.css']
+  styleUrls: ['./alarm.component.css'],
+  animations: [
+    trigger('displayState', [
+      state('none', style({
+        display: 'none', opacity: '0',
+      })),
+      state('block', style({
+        display: 'block', opacity: '1',
+      })),
+      transition('none => block', animate('100ms linear')),
+      transition('block => none', animate('80ms linear'))
+    ])
+  ]
 })
 export class AlarmComponent implements OnInit {
+  isShow: boolean = false;
 
-  constructor() { }
+  dataSet = [
+    {
+      title: '流量计【00DX34557】200流速超过10m³',
+      fun: 'aa()'
+    },
+    {
+      title: '【00DX34557】200流速超过10m³',
+      fun: 'aa()'
+    },
+    {
+      title: '流量计200流速超过10m³',
+      fun: 'aa()'
+    },
+    {
+      title: '流量计【00DX34557】200流速超过10m³',
+      fun: 'aa()'
+    },
+    {
+      title: '流量计【00DX34557】200流速超过10m³',
+      fun: 'aa()'
+    },
+  ];
+
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
