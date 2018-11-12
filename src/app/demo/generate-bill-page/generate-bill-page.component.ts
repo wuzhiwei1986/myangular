9fc506cde0e1551988bdf9ccfdf6621def294f28
wuzhiwei <52383319@qq.com>
2018-11-12 17:29:41 +0800

    .

--- src/app/demo/generate-bill-page/generate-bill-page.component.ts
+++ src/app/demo/generate-bill-page/generate-bill-page.component.ts
@@ -7,6 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GenerateBillPageComponent implements OnInit {
 
+  //表格////////////////////////////////////////////////////////
+
+  isVisibleMiddle = false;
+
+  showModalMiddle(): void {
+    this.isVisibleMiddle = true;
+  }
+
+
+  handleOkMiddle(): void {
+    console.log('click ok');
+    this.isVisibleMiddle = false;
+  }
+
+  handleCancelMiddle(): void {
+    this.isVisibleMiddle = false;
+  }
+
+
+  /**/
+
+  dataSet = [
+    {
+      key    : '1',
+      name   : 'John Brown',
+      age    : 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key    : '2',
+      name   : 'Jim Green',
+      age    : 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key    : '3',
+      name   : 'Joe Black',
+      age    : 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
+
+  //表格 end////////////////////////////////////////////////////////
+
+
   constructor() { }
 
   ngOnInit() {
