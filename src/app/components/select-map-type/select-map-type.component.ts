5c7970355e917741431cb2473bd9661411be3813
wuzhiwei <52383319@qq.com>
2018-11-27 20:44:53 +0800

    .

--- src/app/components/select-map-type/select-map-type.component.ts
+++ src/app/components/select-map-type/select-map-type.component.ts
@@ -1,13 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'ecp-select-map-type',
   templateUrl: './select-map-type.component.html',
-  styleUrls: ['./select-map-type.component.css']
+  styleUrls: ['./select-map-type.component.css'],
+  animations: [
+    trigger('openClose', [
+      transition(':enter', [
+        style({opacity: 0, width: '20px', color: '#000000'}),
+        animate(40, style({opacity: 1, width: '120px', color: '#000000'}))
+      ]),
+      transition(':leave', [
+        animate(40, style({opacity: 0, width: '20px', color: '#ffffff'}))
+      ]),
+
+    ]),
+  ]
 })
 export class SelectMapTypeComponent implements OnInit {
 
-  constructor() { }
+  isShow = false;
+  showList = null;
+
+  mouseOut() {
+    let seft = this;
+    seft.showList = setTimeout(function () {
+      seft.isShow = false;
+    }, 200);
+  }
+
+  cleanTime() {
+    let seft = this;
+    clearTimeout(seft.showList);
+  }
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
