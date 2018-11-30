0926aeebebaf415380d5e05c1f84ee000faeb0ac
zhiwei wu <52383319@qq.com>
2018-11-30 22:07:22 +0800

    .

--- src/app/components/open-details-info2/open-details-info2.component.ts
+++ src/app/components/open-details-info2/open-details-info2.component.ts
@@ -20,7 +20,7 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
 })
 export class OpenDetailsInfo2Component implements OnInit {
   validateForm: FormGroup;
-  isOpen = true;
+  isOpen = false;
   constructor(private fb: FormBuilder) {
   }
 
