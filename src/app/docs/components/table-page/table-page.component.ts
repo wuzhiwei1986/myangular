6d949d09f15c75bb164a3788242b922fe5556ed4
wuzhiwei <52383319@qq.com>
2018-12-03 17:30:13 +0800

    .

--- src/app/docs/components/table-page/table-page.component.ts
+++ src/app/docs/components/table-page/table-page.component.ts
@@ -7,6 +7,7 @@ import * as Mock from 'mockjs';
 })
 export class TablePageComponent implements OnInit {
 
+
   //mock.js 数据
   mockData=Mock.mock({
     'list|1-100': [{
@@ -15,15 +16,23 @@ export class TablePageComponent implements OnInit {
 
     }]
   });
-
-  mockData1=Mock.mock('http://www.testmock.com','get',{
-    'list|1-100': [{
+/**/
+  mockData2=Mock.mock({
+    'list|1-50': [{
       'id|+1': 1,
-      'title': '@title',
-
+      'sysname':'@ctitle(2, 4)',
+      'ordertype':'@ctitle(2, 4)',
+      'ordernum':'@now("yyyyMMdd")',
+      'longtitle':'@ctitle(8, 15)',
+      'area|1': ['华发新城','湾仔','拱北商圈'],
+      'name': '@cname()',
+      'name2': '@cname()',
+      'time': '@datetime',
+      'state|1': ['已完成','处理中'],
+      'result|1': ['正常','异常'],
+      'state2|1': ['处理中','已申请'],
     }]
   });
-
   dataSet = [
     {
       one: '2018-11',
